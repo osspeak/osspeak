c9556dbb8953f2d1d7ec22d5159ec4e33395596f
evfredericksen <evfredericksen@gmail.com>
2016-08-21 22:55:42 -0400

    Revert "test"
    This reverts commit 0e901aebf97fbd667f69cefe9867388f3ef69d4f.

---
+++ engines/RecognizerIO/RecognizerIO/ActionParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecognizerIO
+{
+    static class ActionParser
+    {
+        public static List<Action> BuildActions(string actionText)
+        {
+            var actions = new List<Action>();
+            Action actn;
+            foreach (char ch in actionText)
+            {
+                switch (ch)
+                {
+                    case '`':
+                        actn = new Action();
+                        break;
+                    default:
+                        if (!actions.Any() || actions.Last().Type != "SendKeys")
+                        {
+                            actions.Add(new Action());
+                            actions.Last().Type = "SendKeys";
+                        }
+                        actions.Last().Text += ch;
+                        break;
+                }
+            }
+            return actions;
+        }
+    }
+}
---
+++ engines/RecognizerIO/RecognizerIO/TextToSRG/Api.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToSRG
+{
+    public class Api
+    {
+        public static ExpressionNode TextToAST(string commandString)
+        {
+            var p = new Parser();
+            ExpressionNode astRoot = p.ParseTopLevel(commandString);
+            return astRoot;
+        }
+    }
+}
