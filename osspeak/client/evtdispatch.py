568eac05735ae018655ba03b4b7ac6bf470f6f24
evfredericksen <evfredericksen@gmail.com>
2016-08-26 01:01:43 -0400

    foo

--- osspeak/client/evtdispatch.py
+++ osspeak/client/evtdispatch.py
@@ -9,7 +9,7 @@ class EventDispatcher:
         self.start_module_watcher()
 
     def start_engine_process(self):
-        self.engine_process = ProcessManager(self)
+        self.engine_process = ProcessManager(on_output=self.on_engine_message)
         self.engine_process.start_stdout_listening()
 
     def start_module_watcher(self):
@@ -19,7 +19,12 @@ class EventDispatcher:
         grammar = self.cmd_module_watcher.build_srgs_xml_grammar()
         msg = 'grammar_content {}'.format(ET.tostring(grammar).decode('utf8'))
         self.engine_process.send_message(msg)
-        input('Press the any key: ')
+
+    def on_engine_message(self, msg):
+        split_message = msg.strip().split(' ')
+        if split_message[0] == 'result':
+            action = self.cmd_module_watcher.actions[split_message[1]]
+            action.perform()
 
     def main_loop(self):
         input()
--- osspeak/communication/engines.py
+++ osspeak/communication/engines.py
@@ -5,14 +5,12 @@ ENGINE_PATH = r'C:\Users\evan\modules\OSSpeak\engines\RecognizerIO\RecognizerIO\
 
 class ProcessManager:
 
-    def __init__(self, dispatcher, on_output=lambda x: None):
+    def __init__(self, on_output=lambda x: None):
         self.process = subprocess.Popen(ENGINE_PATH, stdin=subprocess.PIPE,
             stderr=subprocess.PIPE, stdout=subprocess.PIPE,
             creationflags=subprocess.CREATE_NEW_CONSOLE)
-        self.dispatcher = dispatcher
         self.on_output = on_output
         
-
     def send_message(self, msg):
         if not isinstance(msg, bytes):
             msg = msg.encode('utf8')
@@ -25,7 +23,6 @@ class ProcessManager:
         for line in self.process.stdout:
             line = line.decode('utf8')
             self.on_output(line)
-            self.dispatcher.send_message(line)
 
     def start_stdout_listening(self):
         t = threading.Thread(target=self.dispatch_process_output)
