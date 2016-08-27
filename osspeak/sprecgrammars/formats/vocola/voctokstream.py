a108996b76f0dad442c2f96ea103e12a1cf6720d
evfredericksen <evfredericksen@gmail.com>
2016-08-26 22:20:44 -0400

    repetition token

--- osspeak/sprecgrammars/formats/vocola/voctokstream.py
+++ osspeak/sprecgrammars/formats/vocola/voctokstream.py
@@ -4,7 +4,7 @@ from osspeak.sprecgrammars import tokens
 class VocolaTokenStream(abstokenstream.AbstractTokenStream):
 
     def read_next(self):
-        self.read_while(lambda ch: ch in ' \n\t')
+        whitespace = self.read_whitespace()
         if self.stream.eof():
             return
         ch = self.stream.peek()
@@ -13,10 +13,16 @@ class VocolaTokenStream(abstokenstream.AbstractTokenStream):
         if ch == '|':
             return self.read_ortoken()
         if ch in '()':
-            print('asdasd')
             return self.read_paren_token()
+        if ch == '_':
+            if whitespace:
+                self.croak('Repetition must immediately follow a word or grouping')
+            return self.read_repetition()
         self.croak('Invalid character: {}'.format(ch))
         
+
+    def read_whitespace(self):
+        return self.read_while(lambda ch: ch in ' \n\t')
 
     def read_word(self):
         val = self.read_while(lambda ch: ch.isalpha())
@@ -26,17 +32,19 @@ class VocolaTokenStream(abstokenstream.AbstractTokenStream):
         self.stream.next()
         return tokens.OrToken()
 
+    def read_repetition(self):
+        '''
+        Ex: left_3 or (hello|goodbye)_3-9 for inclusive ranges
+        Dash indicates range, low/high both optional, low defaults to
+        0, high defaults to infinity
+        '''
+        # skip first underscore
+        assert self.stream.next() == '_'
+        low = self.read_digits()
+
+
     def read_paren_token(self):
         return tokens.ParenToken(self.stream.next())
 
-        # public override Token ReadNext()
-        # {
-        #     ReadWhile(IsWhitespace);
-        #     if (Input.Eof() == true) return null;
-        #     string ch = Input.Peek();
-        #     if (WordRegex.IsMatch(ch)) return ReadWord();
-        #     if (ch == "(" || ch == ")") return ReadParen();
-        #     if (ch == "|") return ReadOr();
-        #     Croak("Invalid Character: " + ch);
-        #     return null;
-        # }
+    def read_digits(self):
+        return self.read_while(lambda ch: ch.isdigit())
