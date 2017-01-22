2da3d9c4afb36b80b520cbba8dadf731bc740d8e
evfredericksen <evfredericksen@gmail.com>
2017-01-22 16:53:12 -0500

    rewrite 4 almost there?

--- osspeak/client/variables.py
+++ osspeak/client/variables.py
@@ -2,55 +2,47 @@ import collections
 from sprecgrammars.rules import astree
 from sprecgrammars.actions import nodes
 
-def get_descendant_ids(rule_node):
-    descendant_ids = []
-    for child in rule_node.children:
-        if isinstance(child, (astree.Rule, astree.GroupingNode)):
-            descendant_ids.extend(get_descendant_ids(child))
-        descendant_ids.append(child.id)
-    return descendant_ids
-
-def build_grouping_map(node, grouping_map=None, ancestor_ids=None):
-    grouping_map = collections.OrderedDict() if grouping_map is None else grouping_map
-    ancestor_ids = [node.id] if ancestor_ids is None else ancestor_ids + [node.id]
-    for child in getattr(node, 'children', []):
-        build_grouping_map(child, grouping_map, ancestor_ids)
-    if isinstance(node, astree.GroupingNode):
-        node_path = tuple(ancestor_ids)
-        assert node_path not in grouping_map
-        grouping_map[node_path] = None
-    return grouping_map
-
+TreeNodeWrapper = collections.namedtuple('TreeNodeWrapper', ['path', 'node', 'rule_path', 'descendant_ids'])
 class RecognitionResultsTree:
 
+
     def __init__(self, engine_variables, root_rule_node):
-
-        self.engine_variables = engine_variables
         self.root_rule_node = root_rule_node
-        self.grouping_ids = [p for p, n in self.tree_nodes() if isinstance(n, astree.GroupingNode)]
-        self.engine_variable_set = set(v[0] for v in self.engine_variables)
-        self.init_actions()
+        self.engine_variables = engine_variables
+        self.init_everything()
 
-    @property
-    def action_variables(self):
-        variables = []
-        for _id in self.grouping_ids:
-            variable_action = nodes.RootAction()
-            variable_action.children = self.actions[_id]
-            variables.append(variable_action)
-        return variables
+    def init_everything(self):
+        full_path_engine_variables = []
+        self.rule_paths = {}
+        # {'ruleid': {'child id': 'child full path'}}
+        self.rule_paths2 = collections.defaultdict(dict)
+        self.tree_node_map = {}
+        for node_wrapper in self.tree_nodes():
+            self.tree_node_map[node_wrapper.path] = node_wrapper
+            if isinstance(node_wrapper.node, astree.Rule):
+                self.rule_paths[node_wrapper.node.id] = node_wrapper
+            self.rule_paths2[node_wrapper.rule_path][node_wrapper.path[-1]] = node_wrapper.path
 
-    def init_actions(self):
-        self.actions = collections.defaultdict(list)
-        paths = [(path, node) for (path, node) in self.tree_nodes() if path[-1] in self.engine_variable_set]
-        for path, node in paths:
-            action = self.leaf_action(node)
-            if action is not None:
-                # add action to path and all ancestor paths
-                for i, ancestor_name in enumerate(path):
-                    ancestor_path = path[:i]
-                    self.actions[ancestor_path].append(action)
-        x=4
+    def magic(self):
+        current_node = None
+        magic_list = []
+        current_rule_wrapper = self.rule_paths[self.root_rule_node.id]
+        for i, (var_id, var_val) in enumerate(self.engine_variables):
+            # rely on all rule ids to be unique
+            if var_id in self.rule_paths:
+                current_rule_wrapper = self.rule_paths[var_id]
+                magic_list.append([current_rule_wrapper.path, ''])
+                continue
+            # go up until we find containing rule
+            while var_id not in current_rule_wrapper.descendant_ids:
+                assert current_rule_wrapper.node is not self.root_rule_node
+                current_rule_wrapper = self.rule_paths[current_rule_wrapper.path[:-1]]
+            print('yay', self.rule_paths2.keys())
+            print('nay', current_rule_wrapper.path[-1])
+            full_path = self.rule_paths2[current_rule_wrapper.path[-1]][var_id]
+            print('fp', full_path)
+        print('ml', magic_list)
+        return magic_list
 
     def leaf_action(self, node):
         if getattr(node, 'action_substitute', None) is not None:
@@ -58,9 +50,34 @@ class RecognitionResultsTree:
         if isinstance(node, astree.WordNode):
            return nodes.LiteralKeysAction(node.text)
 
-    def tree_nodes(self, root=None, root_path=None):
+    def tree_nodes(self, root=None, root_path=None, rule_path=None):
         root = self.root_rule_node if root is None else root
+        rule_path = [] if rule_path is None else rule_path
         path = [root.id] if root_path is None else root_path + [root.id]
         for child in getattr(root, 'children', []):
-            yield from self.tree_nodes(child, path)
-        yield tuple(path), root
+            child_rule_path = rule_path + [root.id] if isinstance(root, astree.Rule) else rule_path 
+            yield from self.tree_nodes(child, path, child_rule_path)
+        descendant_ids = set(self.get_descendant_ids(root))
+        yield TreeNodeWrapper(tuple(path), root, tuple(rule_path), descendant_ids)
+
+    def get_descendant_ids(self, node):
+        descendant_ids = []
+        for child in getattr(node, 'children', []):
+            descendant_ids.extend(self.get_descendant_ids(child) + [child.id])
+        return descendant_ids
+
+    @property
+    def action_variables(self):
+        variables = []
+        full_path_engine_variables = self.magic()
+        current_rule = self.root_rule_node
+        for i, (var_id, var_text) in enumerate(self.engine_variables):
+            full_path = magic()
+            node = self.tree_node_map[full_path]
+            action = self.leaf_action(node)
+            if action is not None:
+                for j, node_id in enumerate(full_path, start=1):
+                    # 'abcd' => 'a' 'ab', 'abc', 'abcd'
+                    partial_path = full_path[:j]
+                    self.everything[partial_path].append(action)
+        return variables
