156141da744cefdbc82410f9cce7a241abb439bc
evfredericksen <evfredericksen@gmail.com>
2017-01-22 21:43:47 -0500

    variable rewrite working for now

--- osspeak/client/variables.py
+++ osspeak/client/variables.py
@@ -3,8 +3,8 @@ from sprecgrammars.rules import astree
 from sprecgrammars.actions import nodes
 
 TreeNodeWrapper = collections.namedtuple('TreeNodeWrapper', ['path', 'node', 'rule_path', 'descendant_ids'])
-class RecognitionResultsTree:
 
+class RecognitionResultsTree:
 
     def __init__(self, engine_variables, root_rule_node):
         self.root_rule_node = root_rule_node
@@ -12,16 +12,21 @@ class RecognitionResultsTree:
         self.init_everything()
 
     def init_everything(self):
-        full_path_engine_variables = []
         self.rule_paths = {}
         # {'ruleid': {'child id': 'child full path'}}
-        self.rule_paths2 = collections.defaultdict(dict)
+        self.rule_children = collections.defaultdict(dict)
+        self.groupings = collections.OrderedDict()
         self.tree_node_map = {}
         for node_wrapper in self.tree_nodes():
             self.tree_node_map[node_wrapper.path] = node_wrapper
+            assert isinstance(node_wrapper.rule_path, tuple)
             if isinstance(node_wrapper.node, astree.Rule):
                 self.rule_paths[node_wrapper.node.id] = node_wrapper
-            self.rule_paths2[node_wrapper.rule_path][node_wrapper.path[-1]] = node_wrapper.path
+            if isinstance(node_wrapper.node, astree.GroupingNode):
+                self.groupings[node_wrapper.path] = node_wrapper                
+            self.rule_children[node_wrapper.rule_path][node_wrapper.path[-1]] = node_wrapper.path
+        for k in self.rule_children:
+            assert isinstance(k, tuple)
 
     def magic(self):
         current_node = None
@@ -37,11 +42,8 @@ class RecognitionResultsTree:
             while var_id not in current_rule_wrapper.descendant_ids:
                 assert current_rule_wrapper.node is not self.root_rule_node
                 current_rule_wrapper = self.rule_paths[current_rule_wrapper.path[:-1]]
-            print('yay', self.rule_paths2.keys())
-            print('nay', current_rule_wrapper.path[-1])
-            full_path = self.rule_paths2[current_rule_wrapper.path[-1]][var_id]
-            print('fp', full_path)
-        print('ml', magic_list)
+            full_path = self.rule_children[current_rule_wrapper.path][var_id]
+            magic_list.append([full_path, var_val])
         return magic_list
 
     def leaf_action(self, node):
@@ -68,16 +70,19 @@ class RecognitionResultsTree:
 
     @property
     def action_variables(self):
-        variables = []
+        results = collections.defaultdict(list)
         full_path_engine_variables = self.magic()
-        current_rule = self.root_rule_node
-        for i, (var_id, var_text) in enumerate(self.engine_variables):
-            full_path = magic()
-            node = self.tree_node_map[full_path]
-            action = self.leaf_action(node)
+        for i, (full_path, var_text) in enumerate(full_path_engine_variables):
+            node_wrapper = self.tree_node_map[full_path]
+            action = self.leaf_action(node_wrapper.node)
             if action is not None:
                 for j, node_id in enumerate(full_path, start=1):
                     # 'abcd' => 'a' 'ab', 'abc', 'abcd'
                     partial_path = full_path[:j]
-                    self.everything[partial_path].append(action)
+                    results[partial_path].append(action)
+        variables = []
+        for grouping_path in self.groupings:
+            action = nodes.RootAction()
+            action.children = results.get(grouping_path, [])
+            variables.append(action)
         return variables
