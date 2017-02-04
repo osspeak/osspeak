725913b85595614c39836d2689195d5f7fc7fdbf
Evan <evfredericksen@gmail.com>
2017-02-03 21:26:56 -0500

    linux f keys

--- osspeak/platforms/linuxconstants.py
+++ osspeak/platforms/linuxconstants.py
@@ -20,4 +20,17 @@ XDOTOOL_KEYMAP = {
     '\\': 'backslash',
     '[': 'bracketleft',
     ']': 'bracketright',
+    'f1': 'F1',
+    'f2': 'F2',
+    'f3': 'F3',
+    'f4': 'F4',
+    'f5': 'F5',
+    'f6': 'F6',
+    'f7': 'F7',
+    'f8': 'F8',
+    'f9': 'F9',
+    'f10': 'F10',
+    'f11': 'F11',
+    'f12': 'F12',
+    
 }
