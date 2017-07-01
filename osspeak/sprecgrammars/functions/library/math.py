454b31ef107dfbd93cb06d651b853b4ceb11a5f7
evfredericksen <evfredericksen@gmail.com>
2017-07-01 19:10:41 -0400

    remove print trace

--- osspeak/sprecgrammars/functions/library/math.py
+++ osspeak/sprecgrammars/functions/library/math.py
@@ -18,7 +18,6 @@ operator_symbols = {
 }
 
 def operation(operator_name, *operands):
-    print('abc', operator, operands)
     if operator_name in operator_symbols:
         operator_name = operator_symbols[operator_name]
     operator_function = getattr(operator, operator_name)
