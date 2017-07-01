91ffeb885c5d4d38649e1b94ebb009ec73b69916
evfredericksen <evfredericksen@gmail.com>
2017-07-01 15:13:08 -0400

    more operator symbols for op()

--- osspeak/sprecgrammars/functions/library/math.py
+++ osspeak/sprecgrammars/functions/library/math.py
@@ -4,9 +4,21 @@ import operator
 operator_symbols = {
     '+': 'add',
     '-': 'sub',
+    '*': 'mul',
+    '/': 'truediv',
+    '//': 'floordiv',
+    '%': 'mod',
+    '**': 'pow',
+    '>=': 'ge',
+    '>': 'gt',
+    '<': 'lt',
+    '<=': 'le',
+    '!=': 'ne',
+    '==': 'eq',
 }
 
 def operation(operator_name, *operands):
+    print('abc', operator, operands)
     if operator_name in operator_symbols:
         operator_name = operator_symbols[operator_name]
     operator_function = getattr(operator, operator_name)
