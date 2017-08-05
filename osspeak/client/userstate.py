d0049dc007054718b39eee71fdc92bfe27d7124d
evfredericksen <evfredericksen@gmail.com>
2017-08-05 11:35:19 -0400

    user state loop refactor

--- osspeak/client/userstate.py
+++ osspeak/client/userstate.py
@@ -5,23 +5,20 @@ from platforms import api
 from communication import messages
 import time
 
-reload_files = False
-reload_grammar = False
-
-def create_message_subscriptions(shutdown_event):
+def create_message_subscriptions(shutdown_event, msg_list):
     messages.subscribe(messages.STOP_MAIN_PROCESS, lambda: shutdown_event.set())
-    messages.subscribe(messages.RELOAD_COMMAND_MODULE_FILES, set_reload_files_flag)
-    messages.subscribe(messages.RELOAD_GRAMMAR, set_reload_grammar_flag)
+    messages.subscribe(messages.RELOAD_COMMAND_MODULE_FILES, set_message(msg_list, messages.RELOAD_COMMAND_MODULE_FILES))
+    messages.subscribe(messages.RELOAD_GRAMMAR, set_message(msg_list, messages.RELOAD_GRAMMAR))
 
 def start_watching_user_state(command_module_watcher):
     command_module_watcher.load_initial_user_state()
     shutdown_event = threading.Event()
-    create_message_subscriptions(shutdown_event)
-    loop_args = (command_module_watcher, shutdown_event)
+    msg_list = [None]
+    create_message_subscriptions(shutdown_event, msg_list)
+    loop_args = (command_module_watcher, shutdown_event, msg_list)
     threading.Thread(target=watch_user_system_state, args=loop_args, daemon=True).start()
 
-def watch_user_system_state(command_module_watcher, shutdown_event):
-    global reload_files
+def watch_user_system_state(command_module_watcher, shutdown_event, msg_list):
     previous_window = None
     previous_state = state_copy()
     while not shutdown_event.isSet():
@@ -29,22 +26,18 @@ def watch_user_system_state(command_module_watcher, shutdown_event):
         different_window = current_window != previous_window
         current_state = state_copy()
         different_user_state = previous_state != current_state
-        if different_window or different_user_state or reload_files or reload_grammar:
+        msg = msg_list[0]
+        if different_window or different_user_state or msg:
             new_active_modules = command_module_watcher.get_active_modules(current_window, current_state)
             if new_active_modules != command_module_watcher.active_modules:
+                reload_files = msg == messages.RELOAD_COMMAND_MODULE_FILES
                 command_module_watcher.load_modules(current_window, current_state, reload_files=reload_files)
-            elif reload_grammar:
+            elif msg == messages.RELOAD_GRAMMAR:
                 command_module_watcher.load_and_send_grammar()
             previous_window = current_window
             previous_state = current_state
-            reload_files = False
-            reload_grammar = False
+            msg_list[0] = None
         shutdown_event.wait(timeout=1)
 
-def set_reload_files_flag():
-    global reload_files
-    reload_files = True
-
-def set_reload_grammar_flag():
-    global reload_grammar
-    reload_grammar = True
+def set_message(msg_list, msg):
+    msg_list[0] = msg
