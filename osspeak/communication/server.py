9c2870748da3b6eca4812a4b45a7c5fda13ce29a
evfredericksen <evfredericksen@gmail.com>
2017-08-12 19:03:15 -0400

    flask app routes cannot be methods

--- osspeak/communication/server.py
+++ osspeak/communication/server.py
@@ -14,48 +14,48 @@ from user.settings import user_settings, get_server_address
 from log import logger
 
 app = flask.Flask(__name__)
+push_message_queue = queue.Queue(maxsize=5)
+
+@app.route('/status')
+def check_status():
+    return ''
+
+@app.route('/poll')
+def client_poll():
+    while True:
+        msg = push_message_queue.get()
+        then = msg.pop('timestamp')
+        print(time.clock() - then)
+        return jsonify(msg)
+
+@app.route('/message', methods=['post'])
+def client_message():
+    common.receive_message(request.data)
+    return ''
+
+def send_message(self, msg):
+    while True:
+        try:
+            push_message_queue.put_nowait(msg)
+        except queue.Full:
+            try:
+                push_message_queue.get_nowait()
+            except queue.Empty:
+                pass
+        else:
+            msg['timestamp'] = time.clock()
+            return
 
 class RemoteEngineServer:
 
     def __init__(self):
-        self.push_message_queue = queue.Queue(maxsize=5)
-        messages.subscribe(messages.PERFORM_COMMANDS, self.send_message) 
+        messages.subscribe(messages.PERFORM_COMMANDS, send_message) 
         self.engine = procs.EngineProcessManager()
-
-    @app.route('/status')
-    def check_status(self):
-        return ''
-
-    @app.route('/poll')
-    def client_poll(self):
-        while True:
-            msg = self.push_message_queue.get()
-            then = msg.pop('timestamp')
-            print(time.clock() - then)
-            return jsonify(msg)
-
-    @app.route('/message', methods=['post'])
-    def client_message(self):
-        common.receive_message(request.data)
-        return ''
 
     def loop_forever(self):
         host, port = get_server_address()
         logger.debug(f'Hosting engine server at {host}:{port}')
         app.run(host=host, port=port, threaded=True)
         
-    def send_message(self, msg):
-        while True:
-            try:
-                self.push_message_queue.put_nowait(msg)
-            except queue.Full:
-                try:
-                    self.push_message_queue.get_nowait()
-                except queue.Empty:
-                    pass
-            else:
-                msg['timestamp'] = time.clock()
-                return
-
     def shutdown(self, *a, **k):
         messages.dispatch_sync(messages.STOP_MAIN_PROCESS)
