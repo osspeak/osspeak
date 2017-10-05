5ede0a87b27d29b34d6b5f67a284dab49d3faba9
evfredericksen <evfredericksen@gmail.com>
2017-10-05 18:45:28 -0400

    delete unused server package

--- osspeak/server/handlers.py
+++
@@ -1,7 +0,0 @@
-def dispatch_message_from_client(engine_process, server, msg, *a, **k):
-    if not isinstance(msg, bytes):
-        msg = msg.encode('utf8')
-    if not msg.endswith(b'\n'):
-        msg += b'\n'
-        engine_process.stdin.write(msg)
-        engine_process.stdin.flush()
