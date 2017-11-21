09693f6c351a541f0751492bc53b054990043114
evfredericksen <evfredericksen@gmail.com>
2017-11-21 11:24:56 -0500

    optional grouping in parser.py

--- osspeak/recognition/rules/parser.py
+++ osspeak/recognition/rules/parser.py
@@ -15,12 +15,16 @@ class RuleParser:
         self.debug = debug
         self.stream = ruletokstream.RuleTokenStream(self.text, defined_functions=defined_functions)
         self.grouping_stack = []
+        self.optional_groupings = set()
+        self.repeated_nodes = set()
         self.token_list = []
         self.parse_map = {
             tokens.WordToken: self.parse_word_token,
             tokens.OrToken: self.parse_or_token,
             tokens.GroupingOpeningToken: self.parse_grouping_opening_token,
             tokens.GroupingClosingToken: self.parse_grouping_closing_token,
+            tokens.OptionalGroupingOpeningToken: self.parse_optional_grouping_opening_token,
+            tokens.OptionalGroupingClosingToken: self.parse_optional_grouping_closing_token,
             tokens.RepetitionToken: self.parse_repetition_token,
             tokens.NamedRuleToken: self.parse_named_rule_token,
             tokens.ActionSubstituteToken: self.parse_action_substitute_token
@@ -32,7 +36,8 @@ class RuleParser:
         for tok in self.stream:
             self.token_list.append(tok)
             self.parse_map[type(tok)](tok)
-        assert len(self.grouping_stack) == 1 and self.grouping_stack[0] is top_level_rule
+        valid_last_grouping = len(self.grouping_stack) == 1 or len(self.grouping_stack) == 2 and not self.grouping_stack[-1].open
+        assert self.grouping_stack[0] is top_level_rule and valid_last_grouping
         return top_level_rule
 
     def parse_word_token(self, tok):
@@ -68,20 +73,26 @@ class RuleParser:
         self.grouping_stack.append(grouping_node)
 
     def parse_grouping_closing_token(self, tok):
+        if self.top in self.optional_groupings:
+            self.croak("Can't match '[' with ')'")
         self.top.open = False
-        self.pop_top_grouping_if_closed()
+
+    def parse_optional_grouping_opening_token(self, tok):
+        self.parse_grouping_opening_token(tok)
+        self.optional_groupings.add(self.top)
+        self.apply_repetition(self.top, 0, 1)
+
+    def parse_optional_grouping_closing_token(self, tok):
+        if self.top not in self.optional_groupings:
+            self.croak("Can't match '(' with ']'")
+        self.top.open = False
 
     def parse_repetition_token(self, tok):
         repeated_node = self.modifiable_node
-        repeated_node.repeat_low = tok.low
-        repeated_node.repeat_high = tok.high
+        self.apply_repetition(repeated_node, tok.low, tok.high)
 
     def parse_action_substitute_token(self, tok):
         self.modifiable_node.action_substitute = tok.action
-
-    def apply_repetition(self, node, low=0, high=None):
-        if low is not None:
-            node.low = low
 
     def pop_top_grouping_if_closed(self):
         if not self.top.open:
@@ -98,3 +109,13 @@ class RuleParser:
         if self.top.children:
             return self.top.children[-1]
         self.croak('No modifiable rule node exists')
+
+    def apply_repetition(self, node, low, high):
+        if node in self.repeated_nodes:
+            self.croak('Cannot apply repetition to node twice')
+        self.repeated_nodes.add(node)
+        node.repeat_low = low
+        node.repeat_high = high
+
+    def croak(self, message):
+        raise RuntimeError(message)
