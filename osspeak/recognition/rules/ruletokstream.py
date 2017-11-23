7036a58bcaa0c73d66438364bbe7ed266fc4ff45
evfredericksen <evfredericksen@gmail.com>
2017-11-23 12:30:09 -0500

    use regex for token stream

--- osspeak/recognition/rules/ruletokstream.py
+++ osspeak/recognition/rules/ruletokstream.py
@@ -1,104 +1,58 @@
+import re
+from inspect import isclass
 from recognition import inputstream
 from recognition.rules import tokens
 
-token_character_map = {
-    '|': tokens.OrToken,
-    '(': tokens.GroupingOpeningToken,
-    ')': tokens.GroupingClosingToken,
-    '[': tokens.OptionalGroupingOpeningToken,
-    ']': tokens.OptionalGroupingClosingToken,
-}
+TOKEN_PATTERNS = tuple((re.compile(p, re.I), _) for p, _ in (
+    (r'[\n\t ]+', tokens.WhitespaceToken),
+    (r'[a-z0-9]+', tokens.WordToken),
+    (r'=.+', lambda text, stream: tokens.ActionSubstituteToken(text[1:], stream.defined_functions)),
+    (r'<[a-z_]+[a-z0-9_]*>', lambda text, _: tokens.NamedRuleToken(text[1:-1])),
+    (r'_((\d*-\d*)|\d+)', lambda text, stream: stream.read_repetition(text[1:])),
+    (r'\|', tokens.OrToken),
+    (r'\(', tokens.GroupingOpeningToken),
+    (r'\)', tokens.GroupingClosingToken),
+    (r'\[', tokens.OptionalGroupingOpeningToken),
+    (r'\]', tokens.OptionalGroupingClosingToken),
+))
 
 class RuleTokenStream:
 
     def __init__(self, text, defined_functions=None):
-        self.stream = inputstream.InputStream(text)
-        self.peeked_token = None
+        self.text = text
         self.defined_functions = {} if defined_functions is None else defined_functions
 
-    def read_next(self):
-        whitespace = self.read_whitespace()
-        if self.stream.eof():
-            return
-        ch = self.stream.peek()
-        if ch.isalnum():
-            return self.read_word()
-        if ch in token_character_map:
-            self.stream.next()
-            return token_character_map[ch]()
-        if ch == '<':
-            return self.read_named_rule()
-        if ch == '=':
-            return self.read_action_substitute()
-        if ch == '_':
-            if whitespace:
-                self.croak('Repetition must immediately follow a word or grouping')
-            return self.read_repetition()
-        self.croak(f'Invalid character: {ch}')
-
-    def read_whitespace(self):
-        return self.read_while(lambda ch: ch in ' \n\t')
-
-    def read_word(self):
-        val = self.read_while(lambda ch: ch.isalnum())
-        return tokens.WordToken(val)
-
-    def read_repetition(self):
+    def read_repetition(self, text):
         '''
         Ex: left_3 or (hello|goodbye)_3-9 for inclusive ranges
         Dash indicates range, low/high both optional, low defaults to
         0, high defaults to infinity
         '''
-        # skip first underscore
-        assert self.stream.next() == '_'
-        low = self.read_digits() or 0
-        high = low
-        if self.stream.peek() == '-':
-            self.stream.next()
-            high = self.read_digits() or None
+        assert text
+        if '-' not in text:
+            low, high = text, text
+        else:
+            low, high = text.split('-')
+            low = low or 0
+            high = high or None
         return tokens.RepetitionToken(low=low, high=high)
 
-    def read_digits(self):
-        return self.read_while(lambda ch: ch.isdigit())
-
-    def read_named_rule(self):
-        var_name = self.read_while(lambda ch: ch != '>')
-        self.stream.next()
-        return tokens.NamedRuleToken(var_name[1:])
-
-    def read_action_substitute(self):
-        assert self.stream.next() == '='
-        remaining_text = self.stream.text[self.stream.pos:]
-        tok = tokens.ActionSubstituteToken(remaining_text, self.defined_functions)
-        # could increase self.stream.stream.pos, but better to use
-        # stream interface
-        for i in range(tok.consumed_char_count):
-            self.stream.next()
-        return tok
-
-    def next(self):
-        tok = self.peeked_token
-        self.peeked_token = None
-        return self.read_next() if tok is None else tok
-
-    def peek(self):
-        if self.peeked_token is not None:
-            return self.peeked_token
-        self.peeked_token = self.read_next()
-        return self.peeked_token
-
-    def eof(self):
-        return self.peek() is None
-
-    def croak(self, text):
-        raise RuntimeError(text)
-
-    def read_while(self, predicate):
-        val = ''
-        while not self.stream.eof() and predicate(self.stream.peek()):
-            val += self.stream.next()
-        return val
+    def read_next_token(self, pos):
+        for pattern, token_creator in TOKEN_PATTERNS:
+            match = pattern.match(self.text, pos=pos) 
+            if match:
+                matched_text = self.text[pos:match.span()[-1]]
+                token = token_creator(matched_text) if isclass(token_creator) else token_creator(matched_text, self)
+                if hasattr(token, 'consumed_char_count'):
+                    pos += token.consumed_char_count + 1
+                else:
+                    pos = match.span()[-1]
+                return token, pos
+        else:
+            self.croak('uh oh')
 
     def __iter__(self):
-        while not self.eof():
-            yield self.next()
+        pos = 0
+        while pos < len(self.text):
+            token, pos = self.read_next_token(pos)
+            yield token
