58f1c24f6c4c2d9e1d5f4529f463bc828eb78aa0
evfredericksen <evfredericksen@gmail.com>
2018-01-13 20:54:57 -0500

    remove namespace from asttransform

--- osspeak/recognition/actions/asttransform.py
+++ osspeak/recognition/actions/asttransform.py
@@ -14,9 +14,8 @@ from recognition.actions import library
 
 class NameToStringTransformer(ast.NodeTransformer):
 
-    def __init__(self, root, namespace, arguments):
+    def __init__(self, root, arguments):
         super().__init__()
-        self.namespace = namespace
         self.arguments = arguments
         self.nodes_to_replace = self.to_replace(root)
         
@@ -33,8 +32,7 @@ class NameToStringTransformer(ast.NodeTransformer):
                     names_to_keep.add(child.id)
                     continue
                 is_call_or_slice = isinstance(node, (ast.Subscript, ast.Attribute)) or (isinstance(node, ast.Call) and node.func is child)
-                if not (is_call_or_slice or 
-                    (child.id in self.namespace and node is not root)):
+                if not is_call_or_slice:
                     nodes_to_replace.add(child)
         return set(n for n in nodes_to_replace if n.id not in names_to_keep)
 
@@ -114,9 +112,8 @@ def node_path(node):
 
 def transform_expression(expr_text, namespace=None, arguments=None):
     arguments = arguments or []
-    namespace = get_builtins() if namespace is None else namespace
     expr = ast.parse(expr_text, mode='eval')
-    new_expr = NameToStringTransformer(expr, namespace, arguments).visit(expr)
+    new_expr = NameToStringTransformer(expr, arguments).visit(expr)
     new_expr = SetLiteralTransformer().visit(new_expr)
     new_expr = LambdaArgTransformer().visit(new_expr)
     new_expr = VariableArgumentTransformer(new_expr).visit(new_expr)
