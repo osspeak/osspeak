831183404b780c2872eaf9fe03679653ed856a20
evfredericksen <evfredericksen@gmail.com>
2018-01-26 19:18:45 -0500

    keyboard "back" shortcut

--- osspeak/recognition/actions/library/keyboard.py
+++ osspeak/recognition/actions/library/keyboard.py
@@ -4,14 +4,24 @@ from platforms import api
 import time
 import threading
 
+shortcuts = {
+    'back': 'backspace'
+}
+
+def add_keyboard_shortcuts(keys):
+    new_keys = []
+    for combo in keys:
+        new_keys.append([shortcuts.get(k, k) for k in combo])
+    return new_keys
+
 def hold(keys):
     api.type_keypresses(keys, direction='down')
 
 def press(keys):
-    keyboard.press(keys)
+    keyboard.press(add_keyboard_shortcuts(keys))
 
 def release(keys):
-    keyboard.release(keys)
+    keyboard.release(add_keyboard_shortcuts(keys))
 
 def press_and_release(keys):
-    keyboard.send(keys)
+    keyboard.send(add_keyboard_shortcuts(keys))
