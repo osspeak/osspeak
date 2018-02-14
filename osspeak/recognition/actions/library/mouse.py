c1dd05f7096287256aef6f029ab27e23502c3622
evfredericksen <evfredericksen@gmail.com>
2018-02-13 20:51:42 -0500

    use mouse package more

--- osspeak/recognition/actions/library/mouse.py
+++ osspeak/recognition/actions/library/mouse.py
@@ -1,14 +1,13 @@
-from platforms import api
 import mouse
 
 def click():
-    api.mouse_click()
+    mouse.click()
 
 def move(x=None, y=None, absolute=True, duration=0):
     mouse.move(x, y, absolute, duration)
 
 def x():
-    return api.get_mouse_location()[0]
+    return mouse.get_position()[0]
 
 def y():
-    return api.get_mouse_location()[1]
+    return mouse.get_position()[1]
