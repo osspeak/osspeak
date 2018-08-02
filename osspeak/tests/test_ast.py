2ff14f5ff63c87d55c4f511f9616ee2a24c21e3a
Evan <evfredericksen@gmail.com>
2018-08-02 00:24:26 -0400

    rule ast tests

--- osspeak/tests/test_ast.py
+++ osspeak/tests/test_ast.py
@@ -1,9 +1,18 @@
 import sys
 sys.path.insert(0, '../osspeak')
-print(sys.path)
 from recognition.rules import astree
 from recognition import rule
 
-def test_foo():
+def test_hello_world():
     r = rule('hello world')
-    assert 1 == 2
+    sequences = r.root.sequences
+    assert len(sequences) == 1
+    assert [type(x) for x in sequences[0]] == [astree.WordNode, astree.WordNode]
+
+def test_grouping_sequences():
+    r = rule("(at sign='@')")
+    sequences = r.root.sequences
+    assert len(r.root.sequences) == 1
+    assert len(r.root.sequences[0]) == 1
+    node_to_test = r.root.sequences[0][0]
+    assert len(node_to_test.sequences) == 1
