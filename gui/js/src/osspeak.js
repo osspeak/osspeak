45b285f1cddfa1a2c3e1981a0e7be904d9b8b96f
Evan <evfredericksen@gmail.com>
2018-09-08 17:51:33 -0400

    remove dead code

--- gui/js/src/osspeak.js
+++
@@ -1,8 +0,0 @@
-const communicator = new MainProcessCommunicator('ws://localhost:8080/websocket');
-
-function init() {
-    ReactDOM.render(
-        <Application />,
-        document.getElementById('root')
-    )
-}
