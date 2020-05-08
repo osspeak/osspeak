d56d1d46c40d0c0a42a82d46aaab6d1c2f8b4e8c
Evan Fredericksen <evfredericksen@gmail.com>
2020-05-07 20:03:57 -0400

    action variable improvement

--- osspeak/recognition/actions/variables.py
+++ osspeak/recognition/actions/variables.py
@@ -11,15 +11,20 @@ class RecognitionResultsTree:
 
     def initialize_fields(self):
         self.node_paths = {}
-        self.variables = collections.OrderedDict()
+        possible_variables = []
         for node_wrapper in self.iterate_subtree(self.root_rule_node):
             if self.is_variable(node_wrapper):
-                self.variables[node_wrapper.path] = node_wrapper.node
+                possible_variables.append(node_wrapper)
             self.node_paths[node_wrapper.path] = node_wrapper.node
+        self.variables = collections.OrderedDict()
+        for wrapper in possible_variables:
+            ancestor_is_variable = any(w in possible_variables for w in wrapper.ancestors)
+            if not ancestor_is_variable:
+                self.variables[wrapper.path] = wrapper.node
 
     def is_variable(self, node_wrapper):
-        for node in node_wrapper.ancestors + (node_wrapper.node,):
-            if getattr(node, 'ignore_ambiguities', False):
+        for wrapper in node_wrapper.ancestors + (node_wrapper,):
+            if getattr(wrapper.node, 'ignore_ambiguities', False):
                 return False
         is_grouping_variable = isinstance(node_wrapper.node, astree.GroupingNode) and len(node_wrapper.node.sequences) > 1
         is_dictation = (isinstance(node_wrapper.node, astree.RuleReference) and
@@ -28,11 +33,10 @@ class RecognitionResultsTree:
         is_variable = is_grouping_variable or is_dictation
         return is_variable or multiple_repeat_possibilities
 
-
     def iterate_subtree(self, node, ancestors=()):
         wrapper = RuleNodeWrapper(node, ancestors, self.node_ids)
         yield wrapper
-        child_ancestors = ancestors + (node,)
+        child_ancestors = ancestors + (wrapper,)
         if isinstance(node, astree.GroupingNode):
             for seq in node.sequences:
                 for child in seq:
@@ -48,4 +52,4 @@ class RuleNodeWrapper:
     def __init__(self, node, ancestors, node_ids):
         self.node = node
         self.ancestors = ancestors
-        self.path = tuple([node_ids[n] for n in ancestors] + [node_ids[node]])
+        self.path = tuple([node_ids[wrapper.node] for wrapper in ancestors] + [node_ids[node]])
